755685be99700759d4c2ce74f128fff5a240f4db
Huakun Shen <huakun.shen@huakunshen.com>
2024-11-25 03:54:24 -0500

    ci: fix

--- packages/grpc/build.ts
+++ packages/grpc/build.ts
@@ -3,9 +3,15 @@ import path from "path"
 import { fileURLToPath } from "url"
 import { $ } from "bun"
 
-await $`rm -rf ./src/protos`
 const filepath = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(filepath)
+
+const srcPath = path.join(__dirname, "src")
+if (!fs.existsSync(srcPath)) {
+	fs.mkdirSync(srcPath)
+}
+
+await $`rm -rf ./src/protos`
 const protosDir = path.join(__dirname, "protos")
 for (const file of fs.readdirSync(protosDir)) {
 	if (file.endsWith(".proto")) {
