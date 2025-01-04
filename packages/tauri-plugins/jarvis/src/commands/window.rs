df713b515a4bce8208efffda1a27576f507650d7
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-04 17:17:52 -0500

    fix: a platform-specific window command

--- packages/tauri-plugins/jarvis/src/commands/window.rs
+++ packages/tauri-plugins/jarvis/src/commands/window.rs
@@ -7,10 +7,13 @@ pub async fn set_transparent_titlebar<R: Runtime>(
     window: tauri::Window<R>,
     window_label: Option<String>,
 ) -> Result<(), String> {
-    let win_label = window_label.unwrap_or(window.label().to_string());
-    match app.get_webview_window(win_label.as_str()) {
-        Some(webview_win) => webview_win.set_transparent_titlebar(true, true),
-        None => return Err("Window not found".to_string()),
+    #[cfg(target_os = "macos")]
+    {
+        let win_label = window_label.unwrap_or(window.label().to_string());
+        match app.get_webview_window(win_label.as_str()) {
+            Some(webview_win) => webview_win.set_transparent_titlebar(true, true),
+            None => return Err("Window not found".to_string()),
+        }
     }
     Ok(())
 }
