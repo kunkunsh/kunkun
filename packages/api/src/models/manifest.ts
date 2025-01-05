04eea0314b40568bbe8e659becca6028ecca1768
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-05 18:31:36 -0500

    feat: add HeadlessCmd to manifest schema

--- packages/api/src/models/manifest.ts
+++ packages/api/src/models/manifest.ts
@@ -1,17 +1,5 @@
 import { FsPermissionSchema } from "tauri-api-adapter/permissions"
-import {
-	array,
-	boolean,
-	enum_,
-	literal,
-	nullable,
-	number,
-	object,
-	optional,
-	string,
-	union,
-	type InferOutput
-} from "valibot"
+import * as v from "valibot"
 import {
 	AllKunkunPermission,
 	FsPermissionScopedSchema,
@@ -29,159 +17,158 @@ export enum OSPlatformEnum {
 	windows = "windows"
 }
 
-export const OSPlatform = enum_(OSPlatformEnum)
-export type OSPlatform = InferOutput<typeof OSPlatform>
+export const OSPlatform = v.enum_(OSPlatformEnum)
+export type OSPlatform = v.InferOutput<typeof OSPlatform>
 const allPlatforms = Object.values(OSPlatformEnum)
-export const TriggerCmd = object({
-	type: union([literal("text"), literal("regex")]),
-	value: string()
+export const TriggerCmd = v.object({
+	type: v.union([v.literal("text"), v.literal("regex")]),
+	value: v.string()
 })
-export type TriggerCmd = InferOutput<typeof TriggerCmd>
+export type TriggerCmd = v.InferOutput<typeof TriggerCmd>
 export enum TitleBarStyleEnum {
 	"visible" = "visible",
 	"transparent" = "transparent",
 	"overlay" = "overlay"
 }
-export const TitleBarStyle = enum_(TitleBarStyleEnum)
+export const TitleBarStyle = v.enum_(TitleBarStyleEnum)
 // JS new WebViewWindow only accepts lowercase, while manifest loaded from Rust is capitalized. I run toLowerCase() on the value before passing it to the WebViewWindow.
 // This lowercase title bar style schema is used to validate and set the type so TypeScript won't complaint
 // export const TitleBarStyleAllLower = z.enum(["visible", "transparent", "overlay"]);
 // export type TitleBarStyleAllLower = z.infer<typeof TitleBarStyleAllLower>;
-export const WindowConfig = object({
-	center: optional(nullable(boolean())),
-	x: optional(nullable(number())),
-	y: optional(nullable(number())),
-	width: optional(nullable(number())),
-	height: optional(nullable(number())),
-	minWidth: optional(nullable(number())),
-	minHeight: optional(nullable(number())),
-	maxWidth: optional(nullable(number())),
-	maxHeight: optional(nullable(number())),
-	resizable: optional(nullable(boolean())),
-	title: optional(nullable(string())),
-	fullscreen: optional(nullable(boolean())),
-	focus: optional(nullable(boolean())),
-	transparent: optional(nullable(boolean())),
-	maximized: optional(nullable(boolean())),
-	visible: optional(nullable(boolean())),
-	decorations: optional(nullable(boolean())),
-	alwaysOnTop: optional(nullable(boolean())),
-	alwaysOnBottom: optional(nullable(boolean())),
-	contentProtected: optional(nullable(boolean())),
-	skipTaskbar: optional(nullable(boolean())),
-	shadow: optional(nullable(boolean())),
+export const WindowConfig = v.object({
+	center: v.optional(v.nullable(v.boolean())),
+	x: v.optional(v.nullable(v.number())),
+	y: v.optional(v.nullable(v.number())),
+	width: v.optional(v.nullable(v.number())),
+	height: v.optional(v.nullable(v.number())),
+	minWidth: v.optional(v.nullable(v.number())),
+	minHeight: v.optional(v.nullable(v.number())),
+	maxWidth: v.optional(v.nullable(v.number())),
+	maxHeight: v.optional(v.nullable(v.number())),
+	resizable: v.optional(v.nullable(v.boolean())),
+	title: v.optional(v.nullable(v.string())),
+	fullscreen: v.optional(v.nullable(v.boolean())),
+	focus: v.optional(v.nullable(v.boolean())),
+	transparent: v.optional(v.nullable(v.boolean())),
+	maximized: v.optional(v.nullable(v.boolean())),
+	visible: v.optional(v.nullable(v.boolean())),
+	decorations: v.optional(v.nullable(v.boolean())),
+	alwaysOnTop: v.optional(v.nullable(v.boolean())),
+	alwaysOnBottom: v.optional(v.nullable(v.boolean())),
+	contentProtected: v.optional(v.nullable(v.boolean())),
+	skipTaskbar: v.optional(v.nullable(v.boolean())),
+	shadow: v.optional(v.nullable(v.boolean())),
 	// theme: optional(nullable(union([literal("light"), literal("dark")]))), // changing theme of one window will change theme of all windows
-	titleBarStyle: optional(nullable(TitleBarStyle)),
-	hiddenTitle: optional(nullable(boolean())),
-	tabbingIdentifier: optional(nullable(string())),
-	maximizable: optional(nullable(boolean())),
-	minimizable: optional(nullable(boolean())),
-	closable: optional(nullable(boolean())),
-	parent: optional(nullable(string())),
-	visibleOnAllWorkspaces: optional(nullable(boolean()))
+	titleBarStyle: v.optional(v.nullable(TitleBarStyle)),
+	hiddenTitle: v.optional(v.nullable(v.boolean())),
+	tabbingIdentifier: v.optional(v.nullable(v.string())),
+	maximizable: v.optional(v.nullable(v.boolean())),
+	minimizable: v.optional(v.nullable(v.boolean())),
+	closable: v.optional(v.nullable(v.boolean())),
+	parent: v.optional(v.nullable(v.string())),
+	visibleOnAllWorkspaces: v.optional(v.nullable(v.boolean()))
 })
-export type WindowConfig = InferOutput<typeof WindowConfig>
-export const CustomUiCmd = object({
-	type: optional(CmdType, CmdType.enum.UiIframe),
-	main: string("HTML file to load, e.g. dist/index.html"),
-	dist: string("Dist folder to load, e.g. dist, build, out"),
-	description: optional(nullable(string("Description of the Command"), ""), ""),
-	devMain: string("URL to load in development to support live reload, e.g. http://localhost:5173/"),
-	name: string("Name of the command"),
-	window: optional(nullable(WindowConfig)),
-	cmds: array(TriggerCmd, "Commands to trigger the UI"),
-	icon: optional(Icon),
-	platforms: optional(
-		nullable(
-			array(OSPlatform, "Platforms available on. Leave empty for all platforms."),
+export type WindowConfig = v.InferOutput<typeof WindowConfig>
+export const BaseCmd = v.object({
+	main: v.string("HTML file to load, e.g. dist/index.html"),
+	description: v.optional(v.nullable(v.string("Description of the Command"), ""), ""),
+	name: v.string("Name of the command"),
+	cmds: v.array(TriggerCmd, "Commands to trigger the UI"),
+	icon: v.optional(Icon),
+	platforms: v.optional(
+		v.nullable(
+			v.array(OSPlatform, "Platforms available on. Leave empty for all platforms."),
 			allPlatforms
 		),
 		allPlatforms
 	)
 })
-export type CustomUiCmd = InferOutput<typeof CustomUiCmd>
+export const CustomUiCmd = v.object({
+	...BaseCmd.entries,
+	type: v.optional(CmdType, CmdType.enum.UiIframe),
+	dist: v.string("Dist folder to load, e.g. dist, build, out"),
+	devMain: v.string(
+		"URL to load in development to support live reload, e.g. http://localhost:5173/"
+	),
+	window: v.optional(v.nullable(WindowConfig))
+})
+export type CustomUiCmd = v.InferOutput<typeof CustomUiCmd>
 
-export const TemplateUiCmd = object({
-	type: optional(CmdType, CmdType.enum.UiWorker),
-	main: string(),
-	name: string(),
-	description: optional(nullable(string("Description of the Command"), ""), ""),
-	window: optional(nullable(WindowConfig)),
-	cmds: array(TriggerCmd),
-	icon: optional(Icon),
-	platforms: optional(
-		nullable(
-			array(OSPlatform, "Platforms available on. Leave empty for all platforms."),
-			allPlatforms
-		),
-		allPlatforms
-	)
+export const TemplateUiCmd = v.object({
+	...BaseCmd.entries,
+	type: v.optional(CmdType, CmdType.enum.UiWorker),
+	window: v.optional(v.nullable(WindowConfig))
 })
-export type TemplateUiCmd = InferOutput<typeof TemplateUiCmd>
-export const PermissionUnion = union([
+export const HeadlessCmd = v.object({
+	...BaseCmd.entries,
+	type: v.optional(CmdType, CmdType.enum.HeadlessWorker)
+})
+export type TemplateUiCmd = v.InferOutput<typeof TemplateUiCmd>
+export const PermissionUnion = v.union([
 	KunkunManifestPermission,
 	FsPermissionScopedSchema,
 	OpenPermissionScopedSchema,
 	ShellPermissionScopedSchema
 ])
-export type PermissionUnion = InferOutput<typeof PermissionUnion>
-export const KunkunExtManifest = object({
-	name: string("Name of the extension (Human Readable)"),
-	shortDescription: string("Description of the extension (Will be displayed in store)"),
-	longDescription: string("Long description of the extension (Will be displayed in store)"),
-	identifier: string(
+export type PermissionUnion = v.InferOutput<typeof PermissionUnion>
+export const KunkunExtManifest = v.object({
+	name: v.string("Name of the extension (Human Readable)"),
+	shortDescription: v.string("Description of the extension (Will be displayed in store)"),
+	longDescription: v.string("Long description of the extension (Will be displayed in store)"),
+	identifier: v.string(
 		"Unique identifier for the extension, must be the same as extension folder name"
 	),
 	icon: Icon,
-	permissions: array(
+	permissions: v.array(
 		PermissionUnion,
 		"Permissions Declared by the extension. e.g. clipboard-all. Not declared APIs will be blocked."
 	),
-	demoImages: array(string("Demo images for the extension")),
-	customUiCmds: array(CustomUiCmd, "Custom UI Commands"),
-	templateUiCmds: array(TemplateUiCmd, "Template UI Commands")
+	demoImages: v.array(v.string("Demo images for the extension")),
+	customUiCmds: v.array(CustomUiCmd, "Custom UI Commands"),
+	templateUiCmds: v.array(TemplateUiCmd, "Template UI Commands"),
+	headlessCmds: v.array(HeadlessCmd, "Headless Commands")
 })
-export type KunkunExtManifest = InferOutput<typeof KunkunExtManifest>
+export type KunkunExtManifest = v.InferOutput<typeof KunkunExtManifest>
 
-const Person = union([
-	object({
-		name: string("GitHub Username"),
-		email: string("Email of the person"),
-		url: optional(nullable(string("URL of the person")))
+const Person = v.union([
+	v.object({
+		name: v.string("GitHub Username"),
+		email: v.string("Email of the person"),
+		url: v.optional(v.nullable(v.string("URL of the person")))
 	}),
-	string("GitHub Username")
+	v.string("GitHub Username")
 ])
 
-export const ExtPackageJson = object({
-	name: string("Package name for the extension (just a regular npm package name)"),
-	version: string("Version of the extension"),
-	author: optional(Person),
-	draft: optional(boolean("Whether the extension is a draft, draft will not be published")),
-	contributors: optional(array(Person, "Contributors of the extension")),
-	repository: optional(
-		union([
-			string("URL of the repository"),
-			object({
-				type: string("Type of the repository"),
-				url: string("URL of the repository"),
-				directory: string("Directory of the repository")
+export const ExtPackageJson = v.object({
+	name: v.string("Package name for the extension (just a regular npm package name)"),
+	version: v.string("Version of the extension"),
+	author: v.optional(Person),
+	draft: v.optional(v.boolean("Whether the extension is a draft, draft will not be published")),
+	contributors: v.optional(v.array(Person, "Contributors of the extension")),
+	repository: v.optional(
+		v.union([
+			v.string("URL of the repository"),
+			v.object({
+				type: v.string("Type of the repository"),
+				url: v.string("URL of the repository"),
+				directory: v.string("Directory of the repository")
 			})
 		])
 	),
 	kunkun: KunkunExtManifest,
-	files: array(string("Files to include in the extension. e.g. ['dist']"))
+	files: v.array(v.string("Files to include in the extension. e.g. ['dist']"))
 })
-export type ExtPackageJson = InferOutput<typeof ExtPackageJson>
+export type ExtPackageJson = v.InferOutput<typeof ExtPackageJson>
 /**
  * Extra fields for ExtPackageJson
  * e.g. path to the extension
  */
-export const ExtPackageJsonExtra = object({
+export const ExtPackageJsonExtra = v.object({
 	...ExtPackageJson.entries,
 	...{
-		extPath: string(),
-		extFolderName: string()
+		extPath: v.string(),
+		extFolderName: v.string()
 	}
 })
 
-export type ExtPackageJsonExtra = InferOutput<typeof ExtPackageJsonExtra>
+export type ExtPackageJsonExtra = v.InferOutput<typeof ExtPackageJsonExtra>
