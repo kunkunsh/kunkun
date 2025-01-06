dceed1782d39ccc503699c6e0d5efd0f22685749
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-06 02:09:27 -0500

    feat: improve on boarding page with deno install, setting, ffmpeg install

---
+++ apps/desktop/src/lib/components/standalone/general-settings.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import HotkeyPick from "@/components/standalone/settings/hotkey-pick.svelte"
+	import { appConfig } from "@/stores"
+	import { Button, Switch } from "@kksh/svelte5"
+</script>
+
+<ul class="rounded-lg border">
+	<li>
+		<span>Launch at Login</span>
+		<Switch bind:checked={$appConfig.launchAtLogin} />
+	</li>
+	<li class="">
+		<span>Hotkey</span>
+		<HotkeyPick />
+	</li>
+	<li>
+		<span>Menu Bar Icon</span>
+		<Switch bind:checked={$appConfig.showInTray} />
+	</li>
+	<li>
+		<span>Hide On Blur</span>
+		<Switch bind:checked={$appConfig.hideOnBlur} />
+	</li>
+	<li>
+		<span>Extension Auto Upgrade</span>
+		<Switch bind:checked={$appConfig.extensionAutoUpgrade} />
+	</li>
+	<li>
+		<span>Dev Extension HMR</span>
+		<Switch bind:checked={$appConfig.hmr} />
+	</li>
+	<li>
+		<span>Join Beta Updates</span>
+		<Switch bind:checked={$appConfig.joinBetaProgram} />
+	</li>
+
+	<li>
+		<span>Developer Mode</span>
+		<Switch bind:checked={$appConfig.developerMode} />
+	</li>
+</ul>
+
+<style scoped>
+	li {
+		@apply flex items-center justify-between border-b px-3 py-3;
+	}
+	ul li:last-child {
+		@apply border-b-0;
+	}
+	li > span {
+		@apply text-sm;
+	}
+</style>
--- apps/desktop/src/routes/app/help/onboarding/+page.svelte
+++ apps/desktop/src/routes/app/help/onboarding/+page.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
+	import GeneralSettings from "@/components/standalone/general-settings.svelte"
 	import DenoInstall from "@/components/standalone/help/deno-install.svelte"
+	import FFmpegInstall from "@/components/standalone/help/ffmpeg-install.svelte"
 	import { appConfig } from "@/stores/appConfig"
 	import { Button } from "@kksh/svelte5"
 	import { goto } from "$app/navigation"
 	import { ArrowRightIcon } from "lucide-svelte"
 	import { onMount } from "svelte"
+	import { fade } from "svelte/transition"
 	import { whereIsCommand } from "tauri-plugin-shellx-api"
+	import { Step } from "./steps"
 
 	let step = $state(0)
+	let denoPath = $state("")
+	let ffmpegPath = $state("")
 
 	onMount(async () => {
-		const denoPath = await whereIsCommand("deno")
-		if (!denoPath) {
-			step = 1
-		}
+		denoPath = await whereIsCommand("deno")
+		ffmpegPath = await whereIsCommand("ffmpeg")
 	})
 
 	function nextStep() {
@@ -21,16 +25,39 @@
 	}
 
 	$effect(() => {
-		if (step === 2) {
+		if (step === Step.DenoInstall) {
+			if (denoPath) {
+				step++
+			}
+		} else if (step === Step.FFmpegInstall) {
+			if (ffmpegPath) {
+				step++
+			}
+		} else if (step > Step.FFmpegInstall) {
 			appConfig.setOnBoarded(true)
 			goto("/app")
 		}
 	})
 </script>
 
-{#if step === 0}{:else if step === 1}
-	<DenoInstall />
-{/if}
-<Button class="absolute bottom-4 right-4" variant="outline" size="icon" onclick={nextStep}>
+<main class="container pt-12">
+	{#if step === Step.Welcome}
+		<h1 class="text-3xl font-bold">Welcome to Kunkun</h1>
+		<p>
+			This is a on boarding page to help you set up Kunkun with some basic settings and optional
+			dependencies.
+		</p>
+		<div>Click <strong>Next</strong> to continue</div>
+	{:else if step === Step.GeneralSettings}
+		<h1 class="text-2xl font-bold">General Settings</h1>
+		<small> You can change these settings later in the settings page. </small>
+		<GeneralSettings />
+	{:else if step === Step.DenoInstall}
+		<DenoInstall />
+	{:else if step === Step.FFmpegInstall}
+		<FFmpegInstall />
+	{/if}
+</main>
+<Button class="fixed bottom-4 right-4" variant="outline" size="icon" onclick={nextStep}>
 	<ArrowRightIcon />
 </Button>
