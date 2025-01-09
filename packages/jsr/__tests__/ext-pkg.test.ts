fb749aa519ab7c4563c9718b6c8cf4ca61eb3dab
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-09 15:03:05 -0500

    feat: add validateJsrPackageAsKunkunExtension function in jsr package

---
+++ packages/jsr/__tests__/ext-pkg.test.ts
@@ -0,0 +1,69 @@
+import { describe, expect, test } from "bun:test"
+import { validateJsrPackageAsKunkunExtension } from "../src"
+
+describe("Validate Jsr package as Kunkun extension", () => {
+	test("Package not signed by GitHub Actions", () => {
+		expect(
+			validateJsrPackageAsKunkunExtension({
+				jsrPackage: {
+					scope: "kunkun",
+					name: "api",
+					version: "0.0.47"
+				},
+				githubUsername: "kunkunsh"
+			})
+		).rejects.toThrow("JSR package is not signed by GitHub Actions")
+	})
+
+	test("Non-existent package", () => {
+		expect(
+			validateJsrPackageAsKunkunExtension({
+				jsrPackage: {
+					scope: "kunkun",
+					name: "non-existent-package",
+					version: "0.0.47"
+				},
+				githubUsername: "kunkunsh"
+			})
+		).rejects.toThrow("JSR package does not exist")
+	})
+
+	test("Package not linked to a GitHub repository", () => {
+		expect(
+			validateJsrPackageAsKunkunExtension({
+				jsrPackage: {
+					scope: "hk",
+					name: "tauri-plugin-network-api",
+					version: "2.0.3-beta.1"
+				},
+				githubUsername: "kunkunsh"
+			})
+		).rejects.toThrow("JSR package is not linked to a GitHub repository")
+	})
+
+	test("GitHub repository owner does not match JSR package owner", () => {
+		expect(
+			validateJsrPackageAsKunkunExtension({
+				jsrPackage: {
+					scope: "kunkun",
+					name: "ext-image-processing",
+					version: "0.0.6"
+				},
+				githubUsername: "kunkunsh" // should be HuakunShen
+			})
+		).rejects.toThrow("GitHub repository owner does not match JSR package owner")
+	})
+
+	test("A valid extension package", async () => {
+		expect(
+			await validateJsrPackageAsKunkunExtension({
+				jsrPackage: {
+					scope: "kunkun",
+					name: "ext-image-processing",
+					version: "0.0.6"
+				},
+				githubUsername: "HuakunShen"
+			})
+		).toBe(true)
+	})
+})
--- packages/jsr/src/index.ts
+++ packages/jsr/src/index.ts
@@ -4,6 +4,8 @@ import {
 	getPackageVersion,
 	type GitHubRepository
 } from "@hk/jsr-client/hey-api-client"
+import { ExtPackageJson } from "@kksh/api/models"
+import * as v from "valibot"
 import type { JsrPackageMetadata, NpmPkgMetadata } from "./models"
 
 client.setConfig({
@@ -128,6 +130,13 @@ export function getJsrNpmPkgMetadata(scope: string, name: string): Promise<NpmPk
 	return fetch(url).then((res) => res.json())
 }
 
+/**
+ * Get the tarball url of a Jsr package
+ * @param scope
+ * @param name
+ * @param version
+ * @returns
+ */
 export async function getNpmPackageTarballUrl(
 	scope: string,
 	name: string,
@@ -147,4 +156,147 @@ export async function getNpmPackageTarballUrl(
 export async function getAllVersionsOfJsrPackage(scope: string, name: string): Promise<string[]> {
 	const metadata = await getJsrNpmPkgMetadata(scope, name)
 	return Object.keys(metadata.versions)
+}
+
+/**
+ * Check if a Jsr package exists
+ * @param scope
+ * @param name
+ * @returns
+ */
+export function jsrPackageExists(scope: string, name: string, version?: string): Promise<boolean> {
+	if (version) {
+		return getPackageVersion({
+			path: {
+				scope,
+				package: name,
+				version
+			}
+		}).then((res) => res.response.ok && res.response.status === 200)
+	}
+	return getPackage({
+		path: {
+			scope,
+			package: name
+		}
+	}).then((res) => res.response.ok && res.response.status === 200)
+}
+
+/**
+ * Get the tarball size of a Jsr package
+ * @param url tarball url, can technically be any url
+ * @returns tarball size in bytes
+ */
+export function getTarballSize(url: string): Promise<number> {
+	return fetch(url, { method: "HEAD" }).then((res) => {
+		if (!(res.ok && res.status === 200)) {
+			throw new Error("Failed to fetch tarball size")
+		}
+		return Number(res.headers.get("Content-Length"))
+	})
+}
+
+/**
+ * Validate a Jsr package as a Kunkun extension
+ * - check if jsr pkg is linked to a github repo
+ * - check if jsr pkg is signed with github action
+ * - check if user's github username is the same as repo's owner name
+ * - check if jsr.json or deno.json has the same version as package.json
+ * - validate package.json format against latest schema
+ * @param payload
+ * @returns
+ */
+export async function validateJsrPackageAsKunkunExtension(payload: {
+	jsrPackage: {
+		scope: string
+		name: string
+		version: string
+	}
+	githubUsername: string
+	tarballSizeLimit?: number
+}): Promise<boolean> {
+	// check if jsr package exists
+	const jsrExists = await jsrPackageExists(
+		payload.jsrPackage.scope,
+		payload.jsrPackage.name,
+		payload.jsrPackage.version
+	)
+	if (!jsrExists) {
+		throw new Error("JSR package does not exist")
+	}
+	/* -------------------------------------------------------------------------- */
+	/*                 check if jsr pkg is linked to a github repo                */
+	/* -------------------------------------------------------------------------- */
+	const githubRepo = await getJsrPackageGitHubRepo(
+		payload.jsrPackage.scope,
+		payload.jsrPackage.name
+	)
+	if (githubRepo === null) {
+		throw new Error("JSR package is not linked to a GitHub repository")
+	}
+	/* -------------------------------------------------------------------------- */
+	/*                check if jsr pkg is signed with github action               */
+	/* -------------------------------------------------------------------------- */
+	const signed = await isSignedByGitHubAction(
+		payload.jsrPackage.scope,
+		payload.jsrPackage.name,
+		payload.jsrPackage.version
+	)
+	if (!signed) {
+		throw new Error("JSR package is not signed by GitHub Actions")
+	}
+	/* -------------------------------------------------------------------------- */
+	/*      check if user's github username is the same as repo's owner name      */
+	/* -------------------------------------------------------------------------- */
+	if (githubRepo.owner?.toLowerCase() !== payload.githubUsername.toLowerCase()) {
+		throw new Error(
+			`GitHub repository owner does not match JSR package owner: ${githubRepo.owner} !== ${payload.githubUsername}`
+		)
+	}
+	/* -------------------------------------------------------------------------- */
+	/*     check if jsr.json or deno.json has the same version as package.json    */
+	/* -------------------------------------------------------------------------- */
+	const packageJsonContent = await getJsrPackageSrcFile(
+		payload.jsrPackage.scope,
+		payload.jsrPackage.name,
+		payload.jsrPackage.version,
+		"package.json"
+	)
+	if (!packageJsonContent) {
+		throw new Error("Could not find package.json in JSR package")
+	}
+	let packageJson: any
+	try {
+		packageJson = JSON.parse(packageJsonContent)
+	} catch (error) {
+		throw new Error("Failed to parse package.json")
+	}
+	if (packageJson.version !== payload.jsrPackage.version) {
+		// no need to fetch jsr.json or deno.json content, as we already know the version is valid with JSR API
+		throw new Error("Package version in package.json does not match JSR package version")
+	}
+
+	/* -------------------------------------------------------------------------- */
+	/*             validate package.json format against latest schema             */
+	/* -------------------------------------------------------------------------- */
+	const parseResult = v.safeParse(ExtPackageJson, packageJson)
+	if (!parseResult.success) {
+		throw new Error("package.json format not valid", { cause: v.flatten(parseResult.issues) })
+	}
+	const tarballUrl = await getNpmPackageTarballUrl(
+		payload.jsrPackage.scope,
+		payload.jsrPackage.name,
+		payload.jsrPackage.version
+	)
+	if (!tarballUrl) {
+		throw new Error("Could not get tarball URL for JSR package")
+	}
+	const tarballSize = await getTarballSize(tarballUrl)
+	const sizeLimit = payload.tarballSizeLimit ?? 50 * 1024 * 1024 // default to 50MB
+	if (tarballSize > sizeLimit) {
+		throw new Error(
+			`Package tarball size (${tarballSize} bytes) exceeds limit of ${sizeLimit} bytes`
+		)
+	}
+	return true
 }
