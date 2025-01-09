e096e10bc06be3ef140db76bae0aaf2d4fb3823b
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-09 14:43:22 -0500

    feat: enable devtools in release mode, disable right click contextmenu in release mode (#52)

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import { onMount } from "svelte"
 	import "../app.css"
 	import { ModeWatcher, ThemeWrapper } from "@kksh/svelte5"
+	import { dev } from "$app/environment"
+
+	onMount(() => {
+		if (!dev) {
+			document.addEventListener("contextmenu", function (event) {
+				event.preventDefault()
+				console.warn("contextmenu disabled in release mode", event)
+			})
+		}
+	})
 
 	let { children } = $props()
 </script>
