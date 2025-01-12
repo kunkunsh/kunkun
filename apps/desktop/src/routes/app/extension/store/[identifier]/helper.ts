09c840da417f597bd7b982dbc65b14854a962399
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-12 04:01:05 -0500

    fix: handle optional metadata in getInstallExtras for improved extension installation

--- apps/desktop/src/routes/app/extension/store/[identifier]/helper.ts
+++ apps/desktop/src/routes/app/extension/store/[identifier]/helper.ts
@@ -2,13 +2,13 @@ import type { Tables } from "@kksh/api/supabase/types"
 import type { ExtPublishMetadata } from "@kksh/supabase/models"
 
 export async function getInstallExtras(
-	ext: Tables<"ext_publish"> & { metadata: ExtPublishMetadata }
+	ext: Tables<"ext_publish"> & { metadata?: ExtPublishMetadata }
 ): Promise<{ overwritePackageJson?: string }> {
 	const extras: { overwritePackageJson?: string } = {}
-	if (ext.metadata.sourceType) {
-		if (ext.metadata.sourceType === "jsr") {
-			if (ext.metadata.source) {
-				try {	
+	if (ext.metadata?.sourceType) {
+		if (ext.metadata?.sourceType === "jsr") {
+			if (ext.metadata?.source) {
+				try {
 					const res = await fetch(`${ext.metadata.source}/package.json`)
 					const pkgJsonContent = await res.text()
 					extras.overwritePackageJson = pkgJsonContent
