16d9e4f527aea556ea8d1cd795b7fc5c48c1652d
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-18 05:26:39 -0500

    fix: supabase export
    split every item into its own subexport for better debugability

--- packages/supabase/src/index.ts
+++ packages/supabase/src/index.ts
@@ -1,14 +1,11 @@
 import { createClient } from "@supabase/supabase-js"
 import type { Database } from "./database.types"
 
-export function createSB(supabaseUrl: string, supabaseAnonKey: string) {
-	return createClient<Database>(supabaseUrl, supabaseAnonKey, {
-		auth: {
-			flowType: "pkce"
-		}
-	})
-}
-export { SupabaseAPI } from "./api"
+// export function createSB(supabaseUrl: string, supabaseAnonKey: string) {
+// 	return createClient<Database>(supabaseUrl, supabaseAnonKey, {
+// 		auth: {
+// 			flowType: "pkce"
+// 		}
+// 	})
+// }
 
-export type { Database, Tables } from "./database.types"
-export { SBExt } from "./models"
