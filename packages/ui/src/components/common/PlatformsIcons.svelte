646801ac99b9212e6062470c245558551cce38ab
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-21 05:52:55 -0500

    fix: extension store platform display bug

--- packages/ui/src/components/common/PlatformsIcons.svelte
+++ packages/ui/src/components/common/PlatformsIcons.svelte
@@ -1,27 +1,33 @@
 <script lang="ts">
-	import { IconEnum, type OSPlatformEnum } from "@kksh/api/models"
+	import { IconEnum, OSPlatformEnum } from "@kksh/api/models"
 	import { IconMultiplexer } from "@kksh/ui"
 
 	const { platforms }: { platforms: OSPlatformEnum[] } = $props()
 </script>
 
 <div class="flex gap-2">
-	<IconMultiplexer
-		icon={{
-			type: IconEnum.Iconify,
-			value: "logos:linux-tux"
-		}}
-	/>
-	<IconMultiplexer
-		icon={{
-			type: IconEnum.Iconify,
-			value: "basil:apple-solid"
-		}}
-	/>
-	<IconMultiplexer
-		icon={{
-			type: IconEnum.Iconify,
-			value: "devicon:windows8"
-		}}
-	/>
+	{#if platforms.includes(OSPlatformEnum.linux)}
+		<IconMultiplexer
+			icon={{
+				type: IconEnum.Iconify,
+				value: "logos:linux-tux"
+			}}
+		/>
+	{/if}
+	{#if platforms.includes(OSPlatformEnum.macos)}
+		<IconMultiplexer
+			icon={{
+				type: IconEnum.Iconify,
+				value: "basil:apple-solid"
+			}}
+		/>
+	{/if}
+	{#if platforms.includes(OSPlatformEnum.windows)}
+		<IconMultiplexer
+			icon={{
+				type: IconEnum.Iconify,
+				value: "devicon:windows8"
+			}}
+		/>
+	{/if}
 </div>
