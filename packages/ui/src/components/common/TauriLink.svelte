1625324a9fd7698184f453959d9cc79918ccd51b
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-22 13:41:14 -0500

    feat: enhance TauriLink component to support conditional rendering based on Tauri environment
    - Added detection for Tauri environment using the browser variable.
    - Updated the TauriLink component to render a button when in Tauri, and an anchor tag for external links otherwise.
    - Improved user experience by ensuring appropriate link behavior based on the application context.

--- packages/ui/src/components/common/TauriLink.svelte
+++ packages/ui/src/components/common/TauriLink.svelte
@@ -3,6 +3,7 @@
 	import type { Snippet } from "svelte"
 	import type { HTMLAttributes } from "svelte/elements"
 	import { open } from "tauri-plugin-shellx-api"
+	import { browser } from '$app/environment';
 
 	const {
 		href,
@@ -14,17 +15,28 @@
 		children: Snippet
 	} = $props()
 
+	// @ts-ignore
+	const isInTauri = browser ? !!window.__TAURI_INTERNALS__ : false
 	function handleClick() {
 		open(href)
 	}
 </script>
 
-<button
-	class={cn(
-		"text-left font-medium text-blue-600 hover:cursor-pointer hover:underline dark:text-blue-500",
-		className
-	)}
+{#if isInTauri}
+	<button
+		class={cn(
+			"text-left font-medium text-blue-600 hover:cursor-pointer hover:underline dark:text-blue-500",
+			className
+		)}
 	onclick={handleClick}
 >
 	{@render children?.()}
-</button>
+	</button>
+{:else}
+	<a href={href} target="_blank" class={cn(
+		"text-left font-medium text-blue-600 hover:cursor-pointer hover:underline dark:text-blue-500",
+		className
+	)}>
+		{@render children?.()}
+	</a>
+{/if}
