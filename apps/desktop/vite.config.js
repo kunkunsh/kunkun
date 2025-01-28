eec2c92241b64cd2d0165f32029beb1bddf84093
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-28 04:39:40 -0500

    chore: clean up vite config trailing comma

--- apps/desktop/vite.config.js
+++ apps/desktop/vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(async () => ({
 	},
 	build: {
 		minify: true
-	},
+	}
 	// optimizeDeps: {
 	// 	include: ["@supabase/supabase-js"] // trying to fix supabase import error
 	// }
