4221b574c9bf189cd65d8ec18bdd71328ba49694
Huakun Shen <huakun.shen@huakunshen.com>
2025-02-13 23:13:35 -0500

    fix(desktop): global hot key register during init must be after appConfig.init (#113)

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
 	import { ParaglideJS } from "@inlang/paraglide-sveltekit"
 	import { i18n } from "$lib/i18n"
-	import { onMount } from "svelte"
 	import "../app.css"
-	import { init } from "@/utils/init"
 	import { ModeWatcher, ThemeWrapper } from "@kksh/svelte5"
 	import { Toaster } from "svelte-sonner"
-
-	onMount(() => {
-		init()
-	})
 
 	let { children } = $props()
 </script>
