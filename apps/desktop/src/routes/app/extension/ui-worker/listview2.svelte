5fc99ca26c2a8c3246e7caeb97378e7fc951b18d
Huakun <huakun.shen@huakunshen.com>
2025-03-02 12:51:53 -0500

    Fix: listview filter (#225)
    * chore: bump desktop package version to 0.1.30
    
    * chore: increase Node.js memory limit for desktop build process
    
    * chore: configure Node.js memory limit for desktop build
    
    * fix(desktop): list view filter mode

--- apps/desktop/src/routes/app/extension/ui-worker/listview2.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { ListSchema } from "@kunkunapi/src/models"
-
-	let { listViewContent }: { listViewContent: ListSchema.List } = $props()
-	let detailWidth = $derived(listViewContent.detail ? (listViewContent.detail?.width ?? 70) : 0)
-</script>
-
-<div>detailWidth: {detailWidth}</div>
