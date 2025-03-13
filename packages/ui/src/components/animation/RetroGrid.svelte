cc5d9cf3a0a9e7fa0c17797cc881c619f939bf0d
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-13 16:44:18 -0400

    Change RetroGrid bg color

--- packages/ui/src/components/animation/RetroGrid.svelte
+++ packages/ui/src/components/animation/RetroGrid.svelte
@@ -29,6 +29,6 @@
 
 	<!-- Background Gradient -->
 	<div
-		class="absolute inset-0 bg-gradient-to-t from-white to-transparent to-90% dark:from-black"
+		class={cn("absolute inset-0 bg-gradient-to-t from-white to-transparent to-90% dark:from-background")}
 	></div>
 </div>
