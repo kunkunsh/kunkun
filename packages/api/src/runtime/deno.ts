0bca6739a7964bd0ce9db171bb5caa293be6be68
Huakun <huakun.shen@huakunshen.com>
2025-03-18 08:42:39 -0400

    [feat] New sysinfo api, update dep (#251)
    * update tauri-plugin-system-info submodule to latest commit cb32fe8
    
    * Update dependencies to latest versions, including valibot to 1.0.0-rc.4 and kkrpc to 0.2.1 across multiple packages. Bump api package version to 0.1.6.
    
    * Update desktop and API package versions; change development server port and URL

--- packages/api/src/runtime/deno.ts
+++ packages/api/src/runtime/deno.ts
@@ -2,7 +2,7 @@ import { DenoIo, RPCChannel } from "kkrpc"
 
 // deno-lint-ignore no-explicit-any
 export function expose(api: Record<string, any>) {
-	const stdio = new DenoIo(Deno.stdin.readable, Deno.stdout.writable)
+	const stdio = new DenoIo(Deno.stdin.readable)
 	const channel = new RPCChannel(stdio, { expose: api })
 	return channel
 }
