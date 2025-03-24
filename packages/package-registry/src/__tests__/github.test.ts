d8cf220b2cfd0f060bf4ea97cb0d2ac2a6430dda
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-24 01:45:00 -0400

    fix typo

--- packages/package-registry/src/__tests__/github.test.ts
+++ packages/package-registry/src/__tests__/github.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test"
-import { getGitHubRepoNodeId, parseGitHubRepoFromUri } from "../github"
+import { getGitHubRepoMetadata, parseGitHubRepoFromUri } from "../github"
 
 test("parse github repo from uri", () => {
 	expect(parseGitHubRepoFromUri("https://github.com/kunkunsh/kunkun-ext-ossinsight")).toEqual({
@@ -9,7 +9,7 @@ test("parse github repo from uri", () => {
 	expect(() => parseGitHubRepoFromUri("invalid-uri")).toThrow("Invalid GitHub repository URI")
 })
 
-test("get github repo id", async () => {
-	const id = await getGitHubRepoNodeId("kunkunsh", "kunkun")
-	expect(id).toBeDefined()
+test("get github repo metadata", async () => {
+	const metadata = await getGitHubRepoMetadata("kunkunsh", "kunkun-ext-ossinsight")
+	expect(metadata).toBeDefined()
 })
