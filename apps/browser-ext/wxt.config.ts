302a09f0e8032f4d55788d5c61897f5654989675
Huakun Shen <huakun.shen@huakunshen.com>
2025-09-07 01:45:20 -0400

    Adds kkrpc to browser extension
    Adds the `kkrpc` package as a submodule and installs it as a
    dependency for the browser extension. This enables communication
    between the browser extension and other parts of the application.
    
    Updates browser extension's package name.

--- apps/browser-ext/wxt.config.ts
+++ apps/browser-ext/wxt.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, defineWebExtConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
-});
+	srcDir: "src",
+	modules: ["@wxt-dev/module-svelte"],
+	webExt: defineWebExtConfig({
+		disabled: true
+	})
+})
