14f29e4eed144505c97a794f791faa5e0dd4a823
eboye <eboyee@gmail.com>
2026-02-03 23:40:04 +0100

    volume fixes for pipewire

--- packages/tauri-plugins/jarvis/src/syscmds/linux.rs
+++ packages/tauri-plugins/jarvis/src/syscmds/linux.rs
@@ -74,6 +74,46 @@ impl Pactl {
     }
 }
 
+struct Wpctl;
+impl Wpctl {
+    fn cmd_exists() -> bool {
+        Command::new("wpctl").arg("--version").output().is_ok()
+    }
+    fn set_volume(value: String) -> anyhow::Result<()> {
+        let output = Command::new("wpctl")
+            .arg("set-volume")
+            .arg("@DEFAULT_AUDIO_SINK@")
+            .arg(value)
+            .output()?;
+
+        if output.status.success() {
+            Ok(())
+        } else {
+            Err(anyhow::anyhow!("Failed to set volume"))
+        }
+    }
+    /// wpctl set-mute @DEFAULT_AUDIO_SINK@ 1 (mute) or 0 (unmute) or toggle
+    fn mute_related_cmd(cmd: &str) -> anyhow::Result<()> {
+        let mute_value = match cmd {
+            "true" | "mute" => "1",
+            "false" | "unmute" => "0",
+            "toggle" => "toggle",
+            _ => return Err(anyhow::anyhow!("Invalid mute command")),
+        };
+        let output = Command::new("wpctl")
+            .arg("set-mute")
+            .arg("@DEFAULT_AUDIO_SINK@")
+            .arg(mute_value)
+            .output()?;
+
+        if output.status.success() {
+            Ok(())
+        } else {
+            Err(anyhow::anyhow!("Failed to mute"))
+        }
+    }
+}
+
 pub struct SystemCmds;
 impl CommonSystemCmds for SystemCmds {
     /// Run nautilus trash://
@@ -121,37 +161,43 @@ impl CommonSystemCmds for SystemCmds {
     }
 
     fn set_volume(percentage: u8) -> anyhow::Result<()> {
-        if Pactl::cmd_exists() {
+        if Wpctl::cmd_exists() {
+            return Wpctl::set_volume(format!("{}%", percentage));
+        } else if Pactl::cmd_exists() {
             return Pactl::set_volume(format!("{}%", percentage));
         } else if Amixer::cmd_exists() {
             return Amixer::set_volume(format!("{}%", percentage));
         } else {
             return Err(anyhow::anyhow!(
-                "No volume control command found (Only Support pactl and amixer)"
+                "No volume control command found (Support wpctl, pactl and amixer)"
             ));
         }
     }
 
     fn turn_volume_up() -> anyhow::Result<()> {
-        if Pactl::cmd_exists() {
+        if Wpctl::cmd_exists() {
+            return Wpctl::set_volume("10%+".to_string());
+        } else if Pactl::cmd_exists() {
             return Pactl::set_volume("+10%".to_string());
         } else if Amixer::cmd_exists() {
             return Amixer::set_volume("10%+".to_string());
         } else {
             return Err(anyhow::anyhow!(
-                "No volume control command found (Only Support pactl and amixer)"
+                "No volume control command found (Support wpctl, pactl and amixer)"
             ));
         }
     }
 
     fn turn_volume_down() -> anyhow::Result<()> {
-        if Pactl::cmd_exists() {
+        if Wpctl::cmd_exists() {
+            return Wpctl::set_volume("10%-".to_string());
+        } else if Pactl::cmd_exists() {
             return Pactl::set_volume("-10%".to_string());
         } else if Amixer::cmd_exists() {
             return Amixer::set_volume("10%-".to_string());
         } else {
             return Err(anyhow::anyhow!(
-                "No volume control command found (Only Support pactl and amixer)"
+                "No volume control command found (Support wpctl, pactl and amixer)"
             ));
         }
     }
@@ -166,38 +212,44 @@ impl CommonSystemCmds for SystemCmds {
 
     /// amixer -D pulse set Master 1+ toggle
     fn toggle_mute() -> anyhow::Result<()> {
-        if Pactl::cmd_exists() {
+        if Wpctl::cmd_exists() {
+            Wpctl::mute_related_cmd("toggle")
+        } else if Pactl::cmd_exists() {
             Pactl::mute_related_cmd("toggle")
         } else if Amixer::cmd_exists() {
             Amixer::mute_related_cmd("toggle")
         } else {
             Err(anyhow::anyhow!(
-                "No volume control command found (Only Support pactl and amixer)"
+                "No volume control command found (Support wpctl, pactl and amixer)"
             ))
         }
     }
     /// amixer -D pulse set Master 1+ mute
     fn mute() -> anyhow::Result<()> {
-        if Pactl::cmd_exists() {
+        if Wpctl::cmd_exists() {
+            Wpctl::mute_related_cmd("true")
+        } else if Pactl::cmd_exists() {
             Pactl::mute_related_cmd("true")
         } else if Amixer::cmd_exists() {
             Amixer::mute_related_cmd("mute")
         } else {
             Err(anyhow::anyhow!(
-                "No volume control command found (Only Support pactl and amixer)"
+                "No volume control command found (Support wpctl, pactl and amixer)"
             ))
         }
     }
 
     /// amixer -D pulse set Master 1+ unmute
     fn unmute() -> anyhow::Result<()> {
-        if Pactl::cmd_exists() {
+        if Wpctl::cmd_exists() {
+            Wpctl::mute_related_cmd("false")
+        } else if Pactl::cmd_exists() {
             Pactl::mute_related_cmd("false")
         } else if Amixer::cmd_exists() {
             Amixer::mute_related_cmd("unmute")
         } else {
             Err(anyhow::anyhow!(
-                "No volume control command found (Only Support pactl and amixer)"
+                "No volume control command found (Support wpctl, pactl and amixer)"
             ))
         }
     }
